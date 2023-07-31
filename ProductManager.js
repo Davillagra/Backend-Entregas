4c822fee151d5d1913d0a154f4913512e275267f
Davillagra <danielsebastianvillagra@hotmail.com>
2023-07-31 17:10:15 -0300

    Segunda-pre-entrega

--- ProductManager.js
+++ ProductManager.js
@@ -1,14 +1,54 @@
+const fs = require("fs")
+
 class ProductManager {
   constructor() {
-    this.products = []
+    this.path = "./products.json"
+    this.path2 = "./usedIds.json"
   }
-  getProducts = () => {
-    return this.products
+  getProducts = async() => {
+    if(fs.existsSync(this.path)) {
+      const data = await fs.promises.readFile(this.path,"utf-8")
+      const product = JSON.parse(data)
+      return product
+    } else {
+      
+      await fs.promises.writeFile(this.path,"[]")
+    }
   }
-  getProductById = (id) => {
+  
+  addProduct = async (product) => {
+    const products = await this.getProducts()
+    let actualId
+    if(fs.existsSync(this.path2)) {
+      const idData = await fs.promises.readFile(this.path2,"utf-8")
+      actualId = JSON.parse(idData)
+    } else {
+      await fs.promises.writeFile(this.path2,"[1]")
+      actualId = [0]
+    }
+    
+    let codeExist = false
+    products.forEach((e) => {
+      if (e.code === product.code) {
+        codeExist = true
+        return
+      }
+    })
+    if (!codeExist) {
+      product.id = actualId[actualId.length - 1 ] + 1
+      products.push(product)
+      await fs.promises.writeFile(this.path,JSON.stringify(products))
+      actualId.push(product.id)
+      await fs.promises.writeFile(this.path2,JSON.stringify(actualId))
+    } else {
+      console.log("No se puede repetir el codigo en dos productos")
+    }
+  }
+  getProductById = async(id) => {
     let idExist = false
     let prodFound = {}
-    this.products.forEach((e) => {
+    const products = await this.getProducts()
+    products.forEach((e) => {
       if (e.id === id) {
         idExist = true
         return (prodFound = { ...e })
@@ -20,64 +60,64 @@ class ProductManager {
       return `El producto de id ${id} no existe`
     }
   }
-  addProduct = (
-    title,
-    description,
-    price,
-    thumbnail = "Sin imagen",
-    code,
-    stock
-  ) => {
-    const product = {
-      title,
-      description,
-      price,
-      thumbnail,
-      code,
-      stock,
-    }
-    if (this.products.length === 0) {
-      product.id = 1
+  updateProduct = async(id,updateData)=>{
+    const product = await this.getProductById(id)
+    if(typeof product === "string") {
+      return `El producto de id ${id} no existe`
     } else {
-      product.id = this.products[this.products.length - 1].id + 1
+      const products = await this.getProducts()
+      const productUpdated = { ...product, ...updateData}
+      const index = products.findIndex((p)=>p.id === productUpdated.id)
+      products[index] = productUpdated
+      await fs.promises.writeFile(this.path,JSON.stringify(products))
+      return "El producto se actulizó correctamente"
     }
-    let codeExist = false
-    this.products.forEach((e) => {
-      if (e.code === product.code) {
-        codeExist = true
-        return
-      }
-    })
-    if (!codeExist) {
-      this.products.push(product)
-    } else {
-      console.log("No se puede repetir el codigo en dos productos")
+    
+  }
+  
+  deleteProduct = async(id)=>{
+    const product = await this.getProductById(id)
+    if(typeof product === "string") {
+      return `El producto de id ${id} no existe`
+    }else {
+      const products = await this.getProducts()
+      const index = products.findIndex((p)=> p.id === id)
+      products.splice(index,1)
+      await fs.promises.writeFile(this.path,JSON.stringify(products))
+      return "El producto se borró exitosamente"
     }
+    
   }
 }
 
 const productos = new ProductManager()
 
-console.log(productos.getProducts())
+const productsManager = async () => {
 
-productos.addProduct(
-  "producto 1",
-  "este es un prodcuto",
-  500,
-  "thumnail",
-  "codigo 1",
-  6
-)
+const data = {
+  title: "producto 2",
+  description:"este es otro producto",
+  price:200,
+  thumbnail:"thumbnail 2",
+  code:"codigo 2",
+  stock:8
+}
+const updateData = {
+  title: "Nuevo titulo",
+  description:"producto actualizado",
+  price:400,
+  thumbnail:"imagen actualizada",
+  stock:5
+}
+const getProducts = await productos.getProducts()
+//productos.addProduct(data)
+//const getProductById = await productos.getProductById(1)
+//console.log(getProductById)
+//const updateProduct = await productos.updateProduct(5,updateData)
+//console.log(updateProduct)
+//const deleteProduct = await productos.deleteProduct(1)
+//console.log(deleteProduct)
 
-console.log(productos.getProducts())
+}
 
-productos.addProduct(
-  "producto 1",
-  "este es un prodcuto",
-  500,
-  "thumnail",
-  "codigo 1",
-  6
-)
-
-console.log(productos.getProductById(0))
+productsManager()
